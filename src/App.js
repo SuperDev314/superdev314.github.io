2e8f0151bdd408f65487a43f8a8975a2ed3b90dd
SuperDev314 <sdfdfg314@gmail.com>
2023-05-05 11:13:43 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+function App() { 
   const [load, upadateLoad] = useState(true);
 
   useEffect(() => {
